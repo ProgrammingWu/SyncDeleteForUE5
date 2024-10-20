5b815dab80189376537f9dfab97ea8a08f9d2ac9
WuDev <wx_pg45@qq.com>
2024-10-20 13:58:40 +0800

    增加注释

--- SyncDelete/Source/SyncDelete/Private/SyncDeleWindowsTools.h
+++ SyncDelete/Source/SyncDelete/Private/SyncDeleWindowsTools.h
@@ -1,3 +1,11 @@
+/*
+Copyright(c) 2024 裴鸿喆. All Rights Reserved.
+版权所有(c) 2024 裴鸿喆，保留所有权利。
+
+B站：https://space.bilibili.com/381317561
+寿光市鸿喆电脑科技服务中心
+*/
+
 #pragma once
 
 
--- SyncDelete/Source/SyncDelete/Public/SyncDeleteLibrary.h
+++ SyncDelete/Source/SyncDelete/Public/SyncDeleteLibrary.h
@@ -1,4 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+/*
+Copyright(c) 2024 裴鸿喆. All Rights Reserved.
+版权所有(c) 2024 裴鸿喆，保留所有权利。
+
+B站：https://space.bilibili.com/381317561
+寿光市鸿喆电脑科技服务中心
+*/
 
 #pragma once
 
